ec9e730633519716a63246c8efe8d93844c75c78
Gopinath <gopinathbalu07@gmail.com>
2025-11-04 21:59:22 +0530

    Committed By Gopi Initially

---
+++ AdminService_Gopinath/src/main/java/com/tnsif/adminservice/AdminServiceGopinathApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceGopinathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceGopinathApplication.class, args);
+	}
+
+}
