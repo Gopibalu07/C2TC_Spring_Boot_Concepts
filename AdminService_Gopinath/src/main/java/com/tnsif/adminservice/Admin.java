b0a41a6cb9a9db365c72e24ccc4a13bae9f28052
Gopinath <gopinathbalu07@gmail.com>
2025-11-06 06:57:56 +0530

    committed by Gopi

--- AdminService_Gopinath/src/main/java/com/tnsif/adminservice/Admin.java
+++ AdminService_Gopinath/src/main/java/com/tnsif/adminservice/Admin.java
@@ -6,13 +6,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Admin {
 	
 	@Id
-	@Column(name = "id",nullable = false,unique = true)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
 
 	@Column(name = "username",nullable = false)
